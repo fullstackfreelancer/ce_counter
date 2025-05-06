1115ce7d71c89566082b31a58fa4d00a7f14a690
fullstackfreelancer <simonkoehler1978@gmail.com>
2025-05-06 17:51:43 -0500

    Remove file, no longer needed

--- Configuration/TCA/Overrides/pages.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-   'ce_counter',
-   'Configuration/TypoScript/TsConfig/page.typoscript',
-   'Counter: Page Configuration'
-);
