fa4c529e4866ea5262a5432ae1584c7109012afa
fullstackfreelancer <simonkoehler1978@gmail.com>
2025-05-06 22:28:28 -0500

    Add new database fields

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,4 +2,6 @@ CREATE TABLE tt_content (
     counter_time int(11) unsigned DEFAULT '0' NOT NULL,
     counter_stop int(11) unsigned DEFAULT '0' NOT NULL,
     counter_stop_text text,
+    counter_labels varchar(255) DEFAULT '' NOT NULL,
+    counter_format varchar(255) DEFAULT '' NOT NULL,
 );
